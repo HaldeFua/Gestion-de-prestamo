faccb51873e307f801585c1cc2d81af70d06edb7
HaldeFua <93047379+HaldeFua@users.noreply.github.com>
2024-10-29 19:27:25 -0500

    Update ControladorMain.java

--- controller/ControladorMain.java
+++ controller/ControladorMain.java
@@ -6,38 +6,35 @@ public abstract class ControladorMain {
     static Validaciones val = new Validaciones();
 
     //primer programa de la app
-    public static void run(){
-        //Primera vista del programa, el usuario elige entre iniciar sesión o registrarse
-        int vandera = 0;
+        public static void run() {
+        int bandera = 0;
         ControladorEstudiantesIngenieria controladorEstudiantesIngenieria = new ControladorEstudiantesIngenieria();
         do {
-            vandera = val.capturarInt(Vista.inicio());
+            bandera = val.capturarInt(Vista.inicio());
 
-            if (vandera == 1) {
+            if (bandera == 1) {
                 int controlador = 0;
                 do {
-
+                    System.out.println();
                     controlador = val.capturarInt("Seleccionar una opción:\n"
-                            + "1 Estudiante Ingenieria\n" +
+                            + "1 Estudiante Ingeniería\n" +
                             "2 Estudiante Diseño\n" +
                             "3 Salir");
 
-                    System.out.println(controlador);
-
                     if (controlador == 1) {
                         controladorEstudiantesIngenieria.registrar();
-                    } else if (controlador == 2) {
-                        System.out.println("Valor de controlador: " + controlador);
-                        /*String cedula = val.capturarString("Ingrese cedula: ");
-                        if (controladorEstudiantesIngenieria.iniciarSesion(cedula)) {
-                            Vista.menuPrincipal();
-                        } else {
-                            System.out.println("Error: No se pudo iniciar sesión.");
-                        }*/
                     }
                 } while (controlador != 3);
+            } else if (bandera == 2) {
+                String cedula = val.capturarString("Ingrese cédula: ");
+                if (controladorEstudiantesIngenieria.existeEstudiante(cedula)) {
+                    EstudianteIngenieria usuario = controladorEstudiantesIngenieria.buscar(cedula);
+                    Vista.menuIngenieria(usuario);
+                } else {
+                    System.out.println("Error: No se pudo iniciar sesión.");
+                }
             }
-        } while (vandera != 3);
+        } while (bandera != 3);
     }
 
 }
